bc18ce6e063c6c17b921f3cc347b504a2ae93fa1
imanik <imanik007@gmail.com>
2025-11-26 12:19:17 +0200

    debugging

--- backend/routes/leaderboard.js
+++ backend/routes/leaderboard.js
@@ -5,6 +5,13 @@ export const leaderboardRouter = express.Router();
 
 
 leaderboardRouter.post('/entry',leaderboardEntry)
-leaderboardRouter.get('/data',leaderboardData)
+// leaderboardRouter.get('/data',leaderboardData)
 leaderboardRouter.post("/update-progress", leaderboardUpdate)
+
+console.log("Leaderboard router loaded");
+
+leaderboardRouter.get('/data', (req, res) => {
+  console.log("GET /api/leaderboard/data hit");
+  res.json({ message: "Test works!" });
+});
 
